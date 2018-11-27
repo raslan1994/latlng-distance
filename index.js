a8aba5fe7e58ce7c016871a13f4ebcc1b2022990
Raslan Rauff <raslan1994@gmail.com>
2018-11-27 18:55:50 +0530

    code alignments

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
+//Radius of Earth (Mean)
+const R_KM = 6371; //in km
+
+/**
+ * @param deg Value in Degrees
+ * @returns Values in Radians
+ */
+const getRadValue=(deg)=>{
+  return deg * (Math.PI/180)
+}
 module.exports = {
-  R: 6371, //in km 
   /**
-   * @param deg Value in Degrees
-   * @returns Values in Radians
+   * According to the "haversine formula" calculate distance between two coordinates.
+   * @param fl Location From (object should contain lat, lon values)
+   * @param tl Location To (object should contain lat, lon values)
+   * @param r radius of earth
+   * 
+   * @returns Distance
    */
-  getRadValue:(deg)=>{
-    return deg * (Math.PI/180)
+  distanceDiff:(fl,tl,r)=>{
+    let lat1 = fl.lat, lon1 = fl.lon;
+    let lat2 = tl.lat, lon2 = tl.lon;
+    let dLat = getRadValue(lat2-lat1);
+    let dLon = getRadValue(lon2-lon1);
+    let a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(getRadValue(lat1)) * Math.cos(getRadValue(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    let d = r * c; // Distance in km
+    return d;
   },
+
   /**
    * According to the "haversine formula" calculate distance between two coordinates.
    * @param fl Location From (object should contain lat, lon values)
    * @param tl Location To (object should contain lat, lon values)
    * 
-   * @returns Distance (Numeric value)
+   * @returns Distance difference in km
    */
-  distanceDiffInKm:(fl,tl)=>{
-    let _m = module.exports;
-    let lat1 = fl.lat, lon1 = fl.lon;
-    let lat2 = tl.lat, lon2 = tl.lon;
-    let dLat = _m.getRadValue(lat2-lat1);
-    let dLon = _m.getRadValue(lon2-lon1);
-    let a = 
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(_m.getRadValue(lat1)) * Math.cos(_m.getRadValue(lat2)) * 
-      Math.sin(dLon/2) * Math.sin(dLon/2);
-
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    let d = _m.R * c; // Distance in km
-    return d;
-  }
+  distanceDiffInKm:(fl,tl)=> module.exports.distanceDiff(fl,tl,R_KM),
 }
